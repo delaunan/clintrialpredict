d1bb22ce4959f63ee0a1d0f375e06854725a6800
Nicolas Delaunay <delaunay80@gmail.com>
2026-05-14 19:12:20 +0100

    last version fix around module

--- frontend/app.py
+++ frontend/app.py
@@ -21,7 +21,7 @@ from utils.plot import plot_success_gauge, plot_impact_bar, plot_treemap
 # Load environment variables
 load_dotenv()
 
-from frontend.pitch_landing import render_pitch_page
+from pitch_landing import render_pitch_page
 
 
 # ==========================
